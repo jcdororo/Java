86e7f1a26f07755811d6cca5a64a3eafd2146c13
jcdororo <wnwlcks123@gmail.com>
2020-07-02 17:54:24 +0900

    Formoon

---
+++ formoon.java
@@ -0,0 +1,18 @@
+
+public class formoon {
+
+	final static int N = 30;
+	
+	public static void main(String[] args) {
+		
+		for(int i = N; i > 0; i--)
+		{
+			for(int j = i; j > 0; j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
