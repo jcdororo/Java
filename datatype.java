cb5aaf843b2f50b47d794b10a1d7b4ebff602059
jcdororo <wnwlcks123@gmail.com>
2020-07-02 17:53:25 +0900

    Datatype

---
+++ datatype.java
@@ -0,0 +1,33 @@
+
+public class datatype {
+
+	
+	public static void main(String[] args) {
+
+		boolean a = true;
+		System.out.println(a);
+		
+		char b = '9';
+		System.out.println(b);
+		
+		byte c = 127;
+		System.out.println(c);
+		
+		short d = 32767;
+		System.out.println(d);
+		
+		int e = 500;
+		System.out.println(e);
+		
+		long f = 99999;
+		System.out.println(f);
+		
+		float g = 0.132f;
+		System.out.println(g);
+		
+		double h = 0.132;
+		System.out.println(h);
+		
+	}
+
+}
