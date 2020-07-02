ccae80777e5ec2a32e6e3c1721a380ff46b33269
jcdororo <wnwlcks123@gmail.com>
2020-07-02 17:56:35 +0900

    Substring

---
+++ substring.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		System.out.println(name);
+		System.out.println(name.substring(0,4));
+		System.out.println(name.substring(2,7));
+	}
+}
